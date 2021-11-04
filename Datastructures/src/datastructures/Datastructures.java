99746511287386e07fb0b8a7029d2d51c4f6340e
Shawn Ngutu <shawn.ngutu@smartapplicationsgroup.com>
2021-11-04 20:06:25 +0300

    palindrome added

--- Datastructures/src/datastructures/Datastructures.java
+++ Datastructures/src/datastructures/Datastructures.java
@@ -17,48 +17,52 @@ public class Datastructures {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static Palindromes pl = new Palindromes();
+    public static void main(String[] args) {     
         
-    //        Scanner s = new Scanner(System.in);
-    //        System.out.println("Enter the size of the matrix");
-    //        int n = s.nextInt(); // length of mat matrix
-    //        System.out.println("Enter the matrix");
-    //        int mat[][] = new int[n][n];
-    //        for (int i = 0; i < n; i++) {
-    //            for (int j = 0; j < n; j++) {
-    //                mat[i][j] = s.nextInt();
-    //            }
-    //        }
+        //        Scanner s = new Scanner(System.in);
+        //        System.out.println("Enter the size of the matrix");
+        //        int n = s.nextInt(); // length of mat matrix
+        //        System.out.println("Enter the matrix");
+        //        int mat[][] = new int[n][n];
+        //        for (int i = 0; i < n; i++) {
+        //            for (int j = 0; j < n; j++) {
+        //                mat[i][j] = s.nextInt();
+        //            }
+        //        }
 
-    //        System.out.println("Enter the target size");
-    //        int m = s.nextInt(); // length of target matrix
-    //        int target[][] = new int[m][m];
-    //        System.out.println("Enter the elements of target");
-    //        for (int i = 0; i < m; i++) {
-    //            for (int j = 0; j < m; j++) {
-    //                target[i][j] = s.nextInt();
-    //            }
-    //        }
+        //        System.out.println("Enter the target size");
+        //        int m = s.nextInt(); // length of target matrix
+        //        int target[][] = new int[m][m];
+        //        System.out.println("Enter the elements of target");
+        //        for (int i = 0; i < m; i++) {
+        //            for (int j = 0; j < m; j++) {
+        //                target[i][j] = s.nextInt();
+        //            }
+        //        }
 
     
         //matrix to compare to target rotated matrix
-            int[][] mat   = new int[][] {
-                            new int[] { 1, 2, 3, 4, 5 },
-                            new int[] { 6, 7, 8, 9, 10 },
-                            new int[] { 11, 12, 13, 14, 15 },
-                            new int[] { 16, 17, 18, 19, 20 },
-                            new int[] { 21, 22, 23, 24, 25 } };
+        int[][] mat   = new int[][] {
+                        new int[] { 1, 2, 3, 4, 5 },
+                        new int[] { 6, 7, 8, 9, 10 },
+                        new int[] { 11, 12, 13, 14, 15 },
+                        new int[] { 16, 17, 18, 19, 20 },
+                        new int[] { 21, 22, 23, 24, 25 } };
 
-          //target matrix to compare with
-          int[][] target =  new int[][] {
-                            new int[] { 21, 16, 11, 6 ,1 },
-                            new int[] { 22, 17 ,12 ,7, 2},
-                            new int[] { 23, 18 ,13 ,8 ,3 },
-                            new int[] { 24 ,19 ,14 ,9, 4 },
-                            new int[] { 25 ,20 ,15, 10, 5} };
-          
-          System.out.println("matrix can be formed by rotation: "+findRotation(mat, target));
+        //target matrix to compare with
+        int[][] target =  new int[][] {
+                          new int[] { 21, 16, 11, 6 ,1 },
+                          new int[] { 22, 17 ,12 ,7, 2},
+                          new int[] { 23, 18 ,13 ,8 ,3 },
+                          new int[] { 24 ,19 ,14 ,9, 4 },
+                          new int[] { 25 ,20 ,15, 10, 5} };
+
+         // System.out.println("matrix can be formed by rotation: "+findRotation(mat, target));
+         
+        //palindrome
+        String str = "madam";
+        System.out.println(pl._getPalindrome(str));
     }
     
     // function to rotate the matrix.
